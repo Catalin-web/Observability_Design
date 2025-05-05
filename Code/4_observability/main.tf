79ad3c2f3edfc378e5538a2f3fb33de98feb3cd4
Bugnar Catalin <71924999+Catalin-web@users.noreply.github.com>
2025-05-05 12:23:36 +0300

    add all dashboards

--- Code/4_observability/main.tf
+++ Code/4_observability/main.tf
@@ -21,3 +21,59 @@ module "grafana_dashboard" {
   service_name = "grafana"
   dashboard_title = "Grafana"
 }
+
+module "elasticsearch_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "elasticsearch"
+  dashboard_title = "Elasticsearch"
+}
+
+module "fluentd_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "fluentd"
+  dashboard_title = "Fluentd"
+}
+
+module "prometheus_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "prometheus"
+  dashboard_title = "Prometheus"
+}
+
+module "kibana_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "kibana"
+  dashboard_title = "Kibana"
+}
+
+module "microservice1_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "microservice1"
+  dashboard_title = "Microservice1"
+}
+
+module "microservice2_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "microservice2"
+  dashboard_title = "Microservice2"
+}
+
+module "microservice3_dashboard" {
+  source = "../modules/service_dashboard"
+
+  grafana_folder_uid     = module.grafana.grafana_folder_uid
+  service_name = "microservice3"
+  dashboard_title = "Microservice3"
+}
