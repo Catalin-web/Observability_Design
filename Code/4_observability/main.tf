fa4490998763b01315f72c3aacdf15bacc1bdc31
Bugnar Catalin <71924999+Catalin-web@users.noreply.github.com>
2025-05-05 13:02:35 +0300

    clean up

--- Code/4_observability/main.tf
+++ Code/4_observability/main.tf
@@ -1,9 +1,5 @@
 module "grafana" {
   source = "./grafana"
-
-  grafana_base_url       = data.terraform_remote_state.api.outputs.grafana_base_url
-  grafana_admin_username = data.terraform_remote_state.observability_base.outputs.grafana_admin_username
-  grafana_admin_password = data.terraform_remote_state.observability_base.outputs.grafana_admin_password
 }
 
 module "nginx_dashboard" {
